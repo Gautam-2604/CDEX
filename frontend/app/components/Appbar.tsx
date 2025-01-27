09dd5b45ee9ebe2f46e690fe145b6b1aed0146b3
Gautam-2604 <gaurisariag@gmail.com>
2025-01-27 13:53:19 +0530

    initial commit

---
+++ frontend/app/components/Appbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Appbar = () => {
+  return (
+    <div className='px-2 border-b py-2 justify-between flex'>
+      <div>
+        DCEX
+      </div>
+    </div>
+  )
+}
+
+export default Appbar
